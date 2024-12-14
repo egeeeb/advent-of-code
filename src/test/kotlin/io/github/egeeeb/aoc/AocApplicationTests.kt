2bd4f19bab71c5f91d241baca3f57258d646f8c5
oldborn <s.safak.topcu@gmail.com>
2024-12-14 13:36:22 +0100

    initial commit

---
+++ src/test/kotlin/io/github/egeeeb/aoc/AocApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.egeeeb.aoc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AocApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
